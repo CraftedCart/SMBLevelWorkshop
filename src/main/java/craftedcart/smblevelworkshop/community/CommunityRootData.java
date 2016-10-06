e8ee3e485365404ba2ff062b8b0b80cdbd2251be
CraftedCart <CraftedCart@users.noreply.github.com>
2016-10-06 17:31:09 +0100

    Bugfixes and some polishes

--- src/main/java/craftedcart/smblevelworkshop/community/CommunityRootData.java
+++ src/main/java/craftedcart/smblevelworkshop/community/CommunityRootData.java
@@ -12,10 +12,18 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author CraftedCart
@@ -57,6 +65,47 @@ public class CommunityRootData {
                 creatorList.add(new CommunityRepo(repoElement.getAttribute("username"), repoElement.getAttribute("repoName")));
             }
         }
+
+    }
+
+    public static void removeCreator(File xmlFile, ICommunityCreator creator) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+        creatorList.remove(creator);
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+
+        doc.getDocumentElement().normalize();
+
+        NodeList nodeList = doc.getElementsByTagName("creatorList"); //TODO Check creatorList version
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            Element element = (Element) node;
+
+            if (creator instanceof CommunityUser) {
+                NodeList userList = element.getElementsByTagName("user");
+                String username = ((CommunityUser) creator).getUsername();
+
+                for (int j = 0; j < userList.getLength(); j++) {
+                    Node userNode = userList.item(i);
+                    Element userElement = (Element) userNode;
+
+                    if (Objects.equals(userElement.getAttribute("username"), username)) {
+                        userElement.removeChild(userNode);
+
+                        getXMLString(doc);
+                        break;
+                    }
+                }
+
+                throw new NullPointerException("CommunityUser not found in XML file"); //We iterated through the whole XML without finding the user
+
+            } else if (creator instanceof CommunityRepo) {
+                NodeList repoList = element.getElementsByTagName("repo");
+                //TODO Single repos
+            }
+
+        }
     }
 
     public static void parseAnnouncementsListXML(File xmlFile) throws ParserConfigurationException, IOException, SAXException {
@@ -82,6 +131,17 @@ public class CommunityRootData {
                 announcementList.add(new CommunityAnnouncement(title, body));
             }
         }
+    }
+
+    public static String getXMLString(Document doc) throws TransformerException {
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        StreamResult result = new StreamResult(new StringWriter());
+        DOMSource source = new DOMSource(doc);
+        transformer.transform(source, result);
+
+        return result.getWriter().toString();
     }
 
     public static List<ICommunityCreator> getCreatorList() {
--- src/main/java/craftedcart/smblevelworkshop/community/creator/CommunityRepo.java
+++ src/main/java/craftedcart/smblevelworkshop/community/creator/CommunityRepo.java
@@ -1,5 +1,7 @@
 package craftedcart.smblevelworkshop.community.creator;
 
+import java.util.Objects;
+
 /**
  * @author CraftedCart
  *         Created on 04/10/2016 (DD/MM/YYYY)
@@ -12,6 +14,24 @@ public class CommunityRepo implements ICommunityCreator {
     public CommunityRepo(String username, String repoName) {
         this.username = username;
         this.repoName = repoName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode() * repoName.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof CommunityRepo && Objects.equals(username, ((CommunityRepo) obj).getUsername()) && Objects.equals(username, ((CommunityRepo) obj).getRepoName());
     }
 
 }
